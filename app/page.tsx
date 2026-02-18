21d959bee05d7f9fff838a8800ae41bfca03927b
pvp-masterk <imtherealpsx@gmail.com>
2026-02-18 15:03:26 +0500

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <main style={{
+      height: "100vh",
+      background: "black",
+      color: "white",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center"
+    }}>
+      Atlas is loading...
+    </main>
+  )
+}
